d6035bd02bfe4665890de2a14ef7f093800ae529
BiaPena-br <biancapenna2002@gmail.com>
2026-09-01 16:30:56 -0300

    feat: adiciona arquivo de exercicio de sintaxe com estrutura tradicional.

---
+++ modulo01_fundamentos/Sintaxebasica.java
@@ -0,0 +1,11 @@
+package modulo01_fundamentos;
+
+
+// Antes do java 25 se usava a seguinte sintaxe para criar um programa em Java:
+
+public class Sintaxebasica{
+    public static void main(String[] args) {
+        System.out.println("Ola Mundo");
+    }
+}
+
